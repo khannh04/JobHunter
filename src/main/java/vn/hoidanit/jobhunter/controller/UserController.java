67d7f764bb9edbc28379fb8e956cd25e4c5cbb8c
khannh <kn213742@gmail.com>
2026-04-20 19:52:04 +0700

    20/4

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,8 +1,11 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 import java.util.List;
 
@@ -15,31 +18,34 @@ public class UserController {
     }
 
 
-    @PostMapping("/user")
-    public User createNewUser(@RequestBody User postManUser) {
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
         User karicUser = this.userService.handleCreateNewUser(postManUser);
 
-        return karicUser;
+        return ResponseEntity.status(HttpStatus.CREATED).body(karicUser);
     }
 
-    @DeleteMapping("/user/{id}")
-    public  String deleteUser(@PathVariable("id") long id){
+    @DeleteMapping("/users/{id}")
+    public  ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+        if (id <= 0) {
+            throw new IdInvalidException("Id must be greater than 0");
+        }
         this.userService.handleDeleteUser(id);
-        return "delete user with id: " + id;
+        return ResponseEntity.status(HttpStatus.OK).body("Delete user with id: " + id + " successfully");
     }
 
-    @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable("id") long id) {
-        return this.userService.fetchUserById(id);
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchUserById(id));
     }
 
-    @GetMapping("/user")
-    public List<User> getAllUser() {
-        return this.userService.fetchAllUser();
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUser() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUser());
     }
 
-    @PutMapping("/user/{id}")
-    public User updateUser(@PathVariable("id") long id, @RequestBody User user) {
-        return this.userService.updateUser(id, user);
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.updateUser(id, user));
     }
 }
---
+++ src/main/java/vn/hoidanit/jobhunter/util/FormatRestResponse.java
@@ -0,0 +1,36 @@
+package vn.hoidanit.jobhunter.util;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.http.server.ServletServerHttpResponse;
+import vn.hoidanit.jobhunter.domain.RestReponse;
+
+@ControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+        RestReponse<Object> res = new RestReponse<Object>();
+        res.setStatusCode(status);
+        if (status >= 400) {
+            // case error
+            return body;
+        } else {
+            // case success
+            res.setData(body);
+            res.setMessage("Call API successfully");
+        }
+        return res;
+    }
+}
