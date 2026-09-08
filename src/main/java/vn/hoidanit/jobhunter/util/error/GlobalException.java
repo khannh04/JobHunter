a20c0ce845bae0093423eaf26b670463a1dd3fc9
khannh <kn213742@gmail.com>
2026-09-08 16:57:12 +0700

    create and add PermissionException

--- src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
@@ -65,4 +65,14 @@ public class GlobalException {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
+
+    @ExceptionHandler(value = {PermissionException.class})
+    public ResponseEntity<RestResponse<Object>> handlePermissionException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.FORBIDDEN.value());
+        res.setError(ex.getMessage());
+        res.setMessage("FORBIDDEN");
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+    }
 }
