52f61c73cb74f34bbc17e2c5461295bafb4d9cd3
khannh <kn213742@gmail.com>
2026-09-12 23:59:40 +0700

    active async and schedule

--- src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
@@ -2,6 +2,8 @@ package vn.hoidanit.jobhunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //disable security
 //@SpringBootApplication(exclude = {
@@ -9,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 //})
 
- @SpringBootApplication
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class JobhunterApplication {
 
 	public static void main(String[] args) {
