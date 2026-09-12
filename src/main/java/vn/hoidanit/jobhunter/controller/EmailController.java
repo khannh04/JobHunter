e6398844ebcae1d9556b087f8e94e0efa47e06a6
khannh <kn213742@gmail.com>
2026-09-12 23:55:14 +0700

    add and create send email

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -1,24 +1,35 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailController {
     private final EmailService emailService;
+    private final SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService){
+    public EmailController(EmailService emailService, SubscriberService subscriberService){
+        this.subscriberService = subscriberService;
         this.emailService = emailService;
     }
 
     @GetMapping("/email")
     @ApiMessage("Send simple email")
+//    @Scheduled(cron = "*/60 * * * * *")
+//    @Transactional
     public String sendSimpleEmail(){
-        this.emailService.sendSimpleEmail();
+//        this.emailService.sendSimpleEmail();
+//        this.emailService.sendEmailSync("kn213742@gmail.com", "test send email",
+//                "<h1> <b> hello </b> </h1>", false, true);
+//        this.emailService.sendEmailFromTemplateSync("kn213742@gmail.com", "test send email", "job");
+        this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
     }
 }
